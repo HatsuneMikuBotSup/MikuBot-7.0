80cf6df9050f2240a554c61efb62cda9faa4d19d
Hatsune <hatsunemikubotsup@gmail.com>
2023-09-11 17:44:32 +0200

    discord login

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildBans,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers
+    ],
+    partials: [Discord.Partials.Channel],
+});
+
+client.on("ready", () => {
+    console.log("Bot is ready!");
+});
+
+
+client.login(process.env.TOKEN);
